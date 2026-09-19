2a4690df461455ac558402dc633497aba29a6e10
Phan Son <minikie456@gmail.com>
2026-09-19 15:33:35 +0700

    Init backend folder

---
+++ ppswbs_backend/src/main/java/edu/fpt/sep490_g22/ppswbs_backend/PpswbsBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.fpt.sep490_g22.ppswbs_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpswbsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpswbsBackendApplication.class, args);
+	}
+
+}
---
+++ ppswbs_backend/src/test/java/edu/fpt/sep490_g22/ppswbs_backend/PpswbsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fpt.sep490_g22.ppswbs_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpswbsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
